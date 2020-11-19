1a65bc0f8e67a0150116920cbc214814a47fc3e0
macad <macad@MacBook-Air-cua-macad.local>
2020-11-19 11:27:25 +0700

    Add delay to by pass Captcha

--- app.js
+++ app.js
@@ -3,21 +3,34 @@ var Nightmare = require('nightmare'),
   nightmare = Nightmare({show: true});
 
 async function getStar(urls) {
+	var count = 1;
   	for (var i = 0; i < urls.length; i++) {
+  		var delay = Math.floor((Math.random() * 1000) + 500);
+  		
+  		// open 2nd URL, wait to process captcha
+  		if(count==2){
+  			delay += 5000;
+  		}
   		if(urls[i].length < 10) {
   			console.log('invalid URL')
   			continue;
   		}
-		r = await nightmare.goto(urls[i])
-		  .wait(Math.floor((Math.random() * 1000) + 500))
-		  .evaluate(()=>{
-				var sao = document.querySelector('.score-average') ? document.querySelector('.score-average').innerText : "-";
-				var dg = document.querySelector('div.count') ? document.querySelector('div.count').innerText : "-";
-				return sao + ";" + dg;
-		  })
-		console.log(r);
+  		try {
+			r = await nightmare.goto(urls[i])
+			  .wait(delay)
+			  .evaluate(()=>{
+					var sao = document.querySelector('.score-average') ? document.querySelector('.score-average').innerText : "-";
+					var dg = document.querySelector('div.count') ? document.querySelector('div.count').innerText : "-";
+					return sao + ";" + dg;
+			  })
+			console.log(r);
+			count++;
+		} catch (err) {
+			console.error(err);
+		}
 	}
-	nightmare.end();
+	console.log("DONE");
+	//await nightmare.end();
 }
 
 //############ Nodejs read input from stdin
